919267de7b0f2747a100e5e195c54d19ab0bb224
mcv-s <benjamin.pilcher@outlook.com>
2026-09-11 14:52:49 -0400

    Add game functions? Remove them? Idk

--- program.js
+++ program.js
@@ -1,9 +1,47 @@
+// References
+
+const house1 = document.querySelector("[house]");
+const gameWindow = document.querySelector(".game-window");
+
+
+function resizeGame() {
+    gameWindow.style.scale = Math.min(window.innerWidth / 1280, window.innerHeight / 720) * 1;
+};
+
+
+// Update the game window size
+window.addEventListener("resize", (event) => {
+    resizeGame()
+});
+
+resizeGame()
 
 
 
 
-const house = document.querySelector("[house]");
 
-if (house) {
-    house.style.scale = 2;
+
+
+
+
+
+
+
+if (house1.getAttribute("house") == 1) {
+    house1.style.fontSize = "5em";
+    house1.x = 5
+
+    console.log(house1.y)
 }
+
+
+
+
+
+
+
+
+
+
+
+
