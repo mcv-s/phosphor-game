a40cda859ae280dd8cbe601cd34315aed7e13393
mcv-s <benjamin.pilcher@outlook.com>
2026-09-12 09:03:05 -0400

    Update cloud

--- program.js
+++ program.js
@@ -3,9 +3,35 @@
 const house1 = document.querySelector("[house]");
 const gameWindow = document.querySelector(".game-window");
 
+const mouseFollow = document.querySelector(".follow-mouse");
+
+
+
+
+const gameWidth = 1280
+const gameHeight = 720
+
+
+
+// --------------------------------------------------------------------------------
+// Function to get the accurate mouse X and Y within the game's window 
+// along with other default stuff concerning the game window
+
+let gameMouseX = 0;
+let gameMouseY = 0;
+
+window.addEventListener("mousemove", (event) => {
+
+    offsetAmountX = window.innerWidth - (gameWindow.style.scale * gameWidth);
+    offsetAmountY = window.innerHeight - (gameWindow.style.scale * gameHeight);
+    gameMouseX = (event.clientX - offsetAmountX / 2) / gameWindow.style.scale;
+    gameMouseY = (event.clientY - offsetAmountY / 2) / gameWindow.style.scale;
+
+});
+
 
 function resizeGame() {
-    gameWindow.style.scale = Math.min(window.innerWidth / 1280, window.innerHeight / 720) * 1;
+    gameWindow.style.scale = Math.min(window.innerWidth / gameWidth, window.innerHeight / gameHeight) * 1;
 };
 
 
@@ -17,13 +43,13 @@ window.addEventListener("resize", (event) => {
 resizeGame()
 
 
+// --------------------------------------------------------------------------------
 
 
 
 
-window.addEventListener("keydown", (event) => {
-    house1.x += 5;
-});
+
+
 
 
 
@@ -43,6 +69,19 @@ if (house1.getAttribute("house") == 1) {
 
 
 
+
+
+
+
+function gameLoop() {
+
+    mouseFollow.x += (gameMouseX - mouseFollow.x) / 10;
+    mouseFollow.y += (gameMouseY - 50 - mouseFollow.y) / 10;
+
+    requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
 
 
 
